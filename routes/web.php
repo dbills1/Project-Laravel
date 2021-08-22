13f90bd02f2f582d5b3a5aca402c56b526d5a825
dbills1 <billyadam1018@gmail.com>
2021-08-22 19:45:57 +0700

    membuat file baru pada folder resources/views dan public, mengubah tampilan routes web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Zhongli",
+        "email" => "jonglay@gmail.com",
+        "image" => "zhongli.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
