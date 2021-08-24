75a23d1ea73ac35c8b2e39a8fb54ed70ab688d93
dbills1 <billyadam1018@gmail.com>
2021-08-25 03:55:23 +0700

    Pertemuan7, Jam 03:53 25/08/21, Pembuatan Model Post, migrasi pada tabel post, mengisi tabel post dengan mass assignment, belajar route model binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,41 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post 1",
-            "slug" => "judul-post-1",
-            "author" => "Don Bill",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptatem hic quaerat perspiciatis quos harum corporis aspernatur maxime voluptate quibusdam labore cumque, nobis consequuntur.
-                    Neque, nulla tempore distinctio fugit nam maxime, quos itaque aliquam mollitia, praesentium eos voluptatem sequi.
-                    Ex nulla facilis quas fugit perspiciatis veritatis culpa et asperiores iure alias quia voluptatem eveniet ea nostrum,
-                    nam quod tempore esse ad nemo temporibus voluptas, aut officia animi? Amet dignissimos repellat laudantium, ab labore harum fugit,
-                    possimus omnis quidem id dolorum reprehenderit."
-        ],
-        [
-            "title" => "Judul Post 2",
-            "slug" => "judul-post-2",
-            "author" => "Mamad",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Sunt sed sint laudantium id minima tenetur praesentium exercitationem veniam velit eligendi neque deserunt sapiente,
-                    aliquid, recusandae possimus harum rem a quos ullam facilis? V
-                    oluptas sequi amet assumenda eum animi sed autem dolores itaque,
-                    ab repellat nulla ducimus asperiores in suscipit doloremque sunt perspiciatis nesciunt iure inventore consequuntur a
-                    blanditiis dolore nemo deserunt? Sint ducimus nisi sunt. Nisi recusandae libero, voluptates, vitae,
-                    accusamus earum laudantium pariatur odio itaque harum magnam necessitatibus quam molestias enim dolor nam blanditiis voluptatibus.
-                    Illum numquam cupiditate quas, harum recusandae odio, explicabo accusamus dolores excepturi officiis, dolorem error!"
-        ],
-    ];
+    use HasFactory;
 
-    public static function all() {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug){
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
