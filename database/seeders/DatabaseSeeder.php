b0880d695210a139655a30d4a6996451d6953149
dbills1 <billyadam1018@gmail.com>
2021-08-28 22:56:16 +0700

    Pertemuan 10, Penggunakan Factory dan library faker untuk generate data pada seeder secara otomatis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'Don Bill',
-            'email' => 'dbill@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
 
-        User::create([
-            'name' => 'Mamad',
-            'email' => 'mamad@gmail.com',
-            'password' => bcrypt('54321')
-        ]);
+        // User::create([
+        //     'name' => 'Don Bill',
+        //     'email' => 'dbill@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        // User::create([
+        //     'name' => 'Mamad',
+        //     'email' => 'mamad@gmail.com',
+        //     'password' => bcrypt('54321')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -39,68 +41,70 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
-            'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
-            'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
-            'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
-            'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
-            'Unde distinctio rem asperiores possimus?',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
-            'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
-            'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
-            'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
-            'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
-            'Unde distinctio rem asperiores possimus?',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
+        //     'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
+        //     'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
+        //     'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
+        //     'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
+        //     'Unde distinctio rem asperiores possimus?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
-            'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
-            'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
-            'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
-            'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
-            'Unde distinctio rem asperiores possimus?',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
+        //     'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
+        //     'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
+        //     'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
+        //     'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
+        //     'Unde distinctio rem asperiores possimus?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
-            'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
-            'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
-            'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
-            'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
-            'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
-            'Unde distinctio rem asperiores possimus?',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
+        //     'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
+        //     'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
+        //     'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
+        //     'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
+        //     'Unde distinctio rem asperiores possimus?',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, pariatur beatae repudiandae cum vitae nobis odio aperiam,'.
+        //     'earum aspernatur fuga veritatis. Sunt reiciendis a voluptatibus, similique ut exercitationem minima asperiores facilis'.
+        //     'consequatur odit magni dolor libero porro. Doloremque repudiandae veniam officiis, autem facere officia atque. Hic modi'.
+        //     'sunt tenetur quaerat, voluptatibus illo non fuga quasi quae a impedit minima rerum asperiores accusantium sapiente'.
+        //     'tempore quidem at, officiis optio eveniet rem culpa vel nisi dolorum. Ratione a delectus dolor quasi voluptatem'.
+        //     'reprehenderit tenetur eius, temporibus explicabo molestias iure esse nihil odio tempore alias maiores veritatis dolore.'.
+        //     'Unde distinctio rem asperiores possimus?',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
