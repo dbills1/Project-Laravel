5d13609e0750cb41fe10acb7ab826ab9606899a7
dbills1 <billyadam1018@gmail.com>
2021-08-30 13:32:40 +0700

    editing Post Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,7 +24,7 @@ class PostController extends Controller
         }
 
         return view('posts', [
-            "title" => "All Posts",
+            "title" => "All Posts" . $title,
             "active" => 'posts',
             "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
