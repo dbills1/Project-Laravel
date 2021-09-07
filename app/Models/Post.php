ab543e0b41b68a2c71892fe696a94e5d61649399
dbills1 <billyadam1018@gmail.com>
2021-09-07 20:08:39 +0700

    Pertemuan18, Create View Post, Create slug otomatis dengan sluggable, create combo box category pada view create, create text editor dengan TrixEditor, dan tes ngirim data post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
@@ -19,10 +21,10 @@ class Post extends Model
         // versi callback
         $query->when($filters['search'] ?? false, function($query, $search){
             return $query->where(function($query) use ($search){
-                 $query->where('title', 'like', '%' . $search . '%')
+                $query->where('title', 'like', '%' . $search . '%')
                     ->orWhere('body', 'like', '%' . $search . '%');
-             });
-         });
+            });
+        });
 
         $query->when($filters['category'] ?? false, function($query, $category){
             return $query->whereHas('category', function($query) use ($category) {
@@ -50,5 +52,14 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -11,6 +11,16 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
         <!-- Custom styles for this template -->
         <link href="/css/dashboard.css" rel="stylesheet">
+
+        {{-- Trix Editor --}}
+        <link rel="stylesheet" type="text/css" href="/css/trix.css">
+        <script type="text/javascript" src="/js/trix.js"></script>
+
+        <style>
+            trix-toolbar [data-trix-button-group="file-tools"]{
+                display: none;
+            }
+        </style>
 </head>
 
 <body>
